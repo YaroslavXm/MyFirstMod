72c22fe67ea0f5d841a32fbdf8f0bb06102582bd
YaroslavXm <xmstealernotify@gmail.com>
2025-01-25 15:47:40 +0500

    Initial commit

---
+++ src/main/java/io/xmgames/mod/Xmgam3sModClient.java
@@ -0,0 +1,10 @@
+package io.xmgames.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Xmgam3sModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/io/xmgames/mod/Xmgam3sModDataGenerator.java
@@ -0,0 +1,11 @@
+package io.xmgames.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Xmgam3sModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
