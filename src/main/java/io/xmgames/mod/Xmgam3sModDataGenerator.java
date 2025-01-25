e044098cba700624a46ca863f9c058de8259150c
YaroslavXm <xmstealernotify@gmail.com>
2025-01-25 15:51:57 +0500

    Added the 'Very important comment'

--- src/main/java/io/xmgames/mod/Xmgam3sModDataGenerator.java
+++ src/main/java/io/xmgames/mod/Xmgam3sModDataGenerator.java
@@ -3,6 +3,7 @@ package io.xmgames.mod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+// Very important comment
 public class Xmgam3sModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
